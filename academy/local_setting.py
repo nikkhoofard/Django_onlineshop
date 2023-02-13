a21a697db602eeee72d10c663105ca254b512c15
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-02-13 16:52:20 +0330

    add models

--- academy/local_setting.py
+++ academy/local_setting.py
@@ -1,2 +1,7 @@
 SECRET_KEY = 'django-insecure-*l)u@09@foyxf(=r#x$g4t7kur76_8g4zbm+(ly@c+0zu_n#6h'
 DEBUG = True
+DB_NAME = 'postgres'
+DB_PASSWORD = 12356
+DB_HOST = 'localhost'
+DB_USERNAME = ''
+DB_PORT = '5432'
--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductType(models.Model):
+
+    title = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'ProductType'
+        verbose_name_plural = "ProductTypes"
+
+
+class ProductAttribute(models.Model):
+    INTEGER = 1
+    STRING = 2
+    FLOAT = 3
+
+    ATTRIBUTE_TYPES_FIELDS = (
+        (INTEGER, "Integer"),
+        (STRING, "String"),
+        (FLOAT, "Float"),
+    )
+
+    title = models.CharField(max_length=32)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, related_name='attributes')
+    attribute_type = models.PositiveSmallIntegerField(default=INTEGER, choices=ATTRIBUTE_TYPES_FIELDS)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=32)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    product_type = models.ForeignKey(ProductType, on_delete=models.PROTECT)
+    upc = models.BigIntegerField(unique=True)
+    title = models.CharField(max_length=32)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+    brand = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+
+    def __str__(self):
+        return self.title
+
+
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='attribute_valuse')
+    value = models.CharField(max_length=48)
+    attribute = models.ForeignKey(ProductAttribute, on_delete=models.PROTECT, related_name='values')
+
+    def __str__(self):
+        return f"{self.product}({self.value}"
