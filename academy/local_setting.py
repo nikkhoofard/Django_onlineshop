259df1a631fd4e358657aca15030df8139f4afbd
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-02-14 14:52:43 +0330

    complete connect to database

--- academy/local_setting.py
+++ academy/local_setting.py
@@ -1,7 +1,7 @@
 SECRET_KEY = 'django-insecure-*l)u@09@foyxf(=r#x$g4t7kur76_8g4zbm+(ly@c+0zu_n#6h'
 DEBUG = True
-DB_NAME = 'postgres'
-DB_PASSWORD = 12356
+DB_NAME = 'academyy'
+DB_PASSWORD = 'postgres'
 DB_HOST = 'localhost'
-DB_USERNAME = ''
+DB_USERNAME = 'postgres'
 DB_PORT = '5432'
